5757ccb48809bdb015da31508e983157e5975716
Sandile Ndlela <sandile.ndlela@psg.co.za>
2023-08-24 21:22:52 +0200

    Task 3

--- src/app/components/phones/phones.component.ts
+++ src/app/components/phones/phones.component.ts
@@ -1,8 +1,8 @@
-import { AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { PhonesResponse } from 'src/app/models/phonesResponse';
+import { PhonesResponse, Product } from 'src/app/models/phonesResponse';
 import { PhonesService } from 'src/app/services/phones.service';
-import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import { MatPaginator } from '@angular/material/paginator';
 import { tap } from 'rxjs/operators';
 import { MatTableDataSource } from '@angular/material/table';
 
@@ -12,21 +12,22 @@ import { MatTableDataSource } from '@angular/material/table';
   styleUrls: ['./phones.component.css']
 })
 export class PhonesComponent implements OnInit {
-  declare phoneData$: Observable<PhonesResponse>;
-  phoneData: PhonesResponse[] = [];
-  dataSource: MatTableDataSource<PhonesResponse>;
+  phoneData$: Observable<PhonesResponse>;
+  dataSource: MatTableDataSource<Product>;
   displayedColumns: string[] = ['Title', 'Description', 'Price (R)', 'Discount Percentage', 'Rating', 'Brand', 'Thumbnail'];
   constructor(private phoneService: PhonesService) { }
 
   ngOnInit(): void {
-    this.phoneData$ = this.phoneService.getData().pipe(tap((data) => (this.phoneData.push(data))));
-    this.dataSource = new MatTableDataSource<PhonesResponse>(this.phoneData);
-    console.log(this.dataSource.filteredData)
   }
-  
+
   @ViewChild(MatPaginator) paginator: MatPaginator;
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+    this.phoneData$ = this.phoneService.getData().pipe(
+      tap( n => {
+        this.dataSource = new MatTableDataSource<Product>(n.products)
+        this.dataSource.paginator = this.paginator; 
+      })
+    ); 
   }
 
 }
